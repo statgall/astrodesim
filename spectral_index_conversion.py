bfbce591599848248d63dd0c3e645d34e4632156
Erik Drybread <edrybread@gmail.com>
2022-06-23 13:40:39 -0700

    added indent and return to function

--- spectral_index_conversion.py
+++ spectral_index_conversion.py
@@ -14,9 +14,10 @@ def spectral_index_conversion(flux1, flux2, wavelength1, wavelength2):
 #wavelength1 =     #wavelength (in mm) for the first image was captured as
 #wavelength2 =      #wavelength (in mm) for the second image was captured as
 
-flux_rat = math.log10(flux1 / flux2)
+    flux_rat = math.log10(flux1 / flux2)
 
-wavelength_rat = math.log(wavelength1 / wavelength2)
+    wavelength_rat = math.log(wavelength1 / wavelength2)
 
-beta = flux_rat / wavelength_rat #beta is the new array of 'flux intensities' used to create a new image
+    beta = flux_rat / wavelength_rat #beta is the new array of 'flux intensities' used to create a new image
 
+    return beta
