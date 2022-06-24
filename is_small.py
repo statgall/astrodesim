a0745353a017fe4503c166d0a73188ea78a9cc5d
Erik Drybread <edrybread@gmail.com>
2022-06-24 09:15:34 -0700

    added indent under function

--- is_small.py
+++ is_small.py
@@ -2,14 +2,14 @@
 
 def is_small(im1, im2):
 
-size1 = im1.shape[0] * im1.shape[1]
-siez2 = im2.shape[0] * im2.shape[1]
+    size1 = im1.shape[0] * im1.shape[1]
+    siez2 = im2.shape[0] * im2.shape[1]
 
-if size1 < size2:
-     small_im = im1
-     big_im = im2
-else:
-    small_im = im2
-    big_im = im1
+    if size1 < size2:
+        small_im = im1
+        big_im = im2
+    else:
+        small_im = im2
+        big_im = im1
 
-return small_im, big_im
+    return small_im, big_im
