5d3722b5d9dc82e9071978521d41a43d58acfd83
Erik Drybread <edrybread@gmail.com>
2022-06-24 09:16:22 -0700

    fixed type on line 6

--- is_small.py
+++ is_small.py
@@ -3,7 +3,7 @@
 def is_small(im1, im2):
 
     size1 = im1.shape[0] * im1.shape[1]
-    siez2 = im2.shape[0] * im2.shape[1]
+    size2 = im2.shape[0] * im2.shape[1]
 
     if size1 < size2:
         small_im = im1
