a9c226791e6c00a9dfacd8d99d1a3cf35b0a83a2
statgall <ladygallagher@gmail.com>
2022-06-25 13:06:13 -0700

    Updated main to include a class

--- astrodesim/desim.py
+++ astrodesim/desim.py
@@ -4,16 +4,27 @@ from astrodesim import resize_smalltolarge
 from astrodesim import spectral_index_map
 from astrodesim import make_image
 
-def desim(file1, file2):
+class FeedMeFiles(object):
+    """
+    Class contains two fits files of the same image at different wavelengths
+
+    Args:
+        file1 (str): path to fits file with image at one wavelength
+        file2 (str): path to fits file with same image at another wavelength 
+    """
+    def __init__(self, file1, file2):
+        self.file1 = file1
+        self.file2 = file2
+
+def desim(paths):
     """
     Function outputs a dust emission spectral index map
 
     Args: 
-        file1 (str): path to fits file with image at one wavelength
-        file2 (str): path to fits file with same image at another wavelength
+        files (FeedMeFiles): paths to file1 and file2
     """
-    header1, sim_data1 = upload_fits(file1) 
-    header2, sim_data2 = upload_fits(file2)
+    header1, sim_data1 = upload_fits(paths.file1) 
+    header2, sim_data2 = upload_fits(paths.file2)
 
     smallim, deltapix1, new_header, bigim, new_dim, deltapix2, bigheader = is_small(sim_data1, sim_data2, header1, header2)
 
@@ -22,5 +33,3 @@ def desim(file1, file2):
     desim = spectral_index_map(new_im, bigim, new_header, bigheader)
 
     make_image(desim)
-
-# need to update header file to match data changes
